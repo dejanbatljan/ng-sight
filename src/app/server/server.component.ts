0dec7ad654cb31aadb465fea0c35bb0d4f09f3bc
dejanbatljan <dejan@smm.si>
2020-04-09 18:12:18 +0200

    Update server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -12,6 +12,30 @@ export class ServerComponent implements OnInit {
   @Input() serverInput:Server;
 
   ngOnInit(): void {
+    this.SetServerStatus(this.serverInput.isOnline);
   }
 
+  color:string;
+  buttonText:string;
+
+  toggleStatus(onLineStatus:boolean){
+    console.log(this.serverInput.name, '; ', onLineStatus);
+    this.SetServerStatus(!onLineStatus);
+  }
+
+  SetServerStatus(isOnLine:boolean){
+    if(isOnLine)
+    {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    }
+    else
+    {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+
+    }
+  }
 }
