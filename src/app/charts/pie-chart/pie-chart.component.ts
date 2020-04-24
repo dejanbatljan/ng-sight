20106b617fc65fd6fcf84d6161ff0b0f447538da
dejanbatljan <dejan@smm.si>
2020-04-24 09:24:29 +0200

    Add Themes, pie chart functionality

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import _ from 'lodash';
+import { THEME_COLORS } from '../../shared/theme.colors';
 
+const theme = 'Bright'
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -9,16 +12,35 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
-  pieChartData: number[] = [350,450,120];
-  pieChartLabels: string[] = ['XYZ logistics', 'Main St. Bakery', 'Acme Inc.'];
+  @Input() inputData; any;
+  @Input() limit: number;
+
+  pieChartData: number[]; // = [350,450,120];
+  pieChartLabels: string[]; // = ['XYZ logistics', 'Main St. Bakery', 'Acme Inc.'];
   colors: any[] = [
     {
-      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
+      backgroundColor: this.themeColors(theme), // ['#26547c', '#ff6b6b', '#ffd166']
+      borderColor: '#111'
     }
   ];
-  pieChartType = 'pie';
+  pieChartType = 'doughnut';
 
   ngOnInit(): void {
+    this.parceChartData(this.inputData, this.limit);
+  }
+
+  parceChartData(res:any, limit?:number)
+  {
+    const allData = res.slice(0,limit);
+    this.pieChartData = allData.map(x =>_.values(x)[1]);  //uporaba lodash knjižnice, da dostopaš do lista preko indeka namesto imena
+    this.pieChartLabels = allData.map(x => _.values(x)[0]);
+  }
+
+  themeColors(setName:string): string[] {
+    const c = THEME_COLORS.slice(0)
+    .find(set => set.name === setName).colorSet;
+
+    return c;
   }
 
 }
